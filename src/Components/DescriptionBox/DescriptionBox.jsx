f84156d10b909e6daed20a559579fb2241eb5642
RobelBira <robelbira@gmail.com>
2023-11-01 09:57:33 +0300

    no

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,25 @@
+import "./DescriptionBox.css";
+
+
+
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">
+            Description
+        </div>
+        <div className="descriptionbox-nav-box fade">
+            Reviews (122)
+        </div>
+      </div>
+
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil error laborum earum magni eaque provident nesciunt esse quasi distinctio quos deleniti, nostrum praesentium iure quis possimus facere eveniet! Quod, fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi excepturi aperiam molestias voluptatibus molestiae eius saepe illum aspernatur quibusdam totam amet blanditiis quaerat incidunt facere possimus officia, placeat libero rerum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem voluptas velit, laboriosam atque iste ea aperiam numquam deserunt obcaecati accusantium quam et fugit, ipsum, ut non optio quas sit aut.</p>
+      </div>
+    </div>
+  );
+}
+export default DescriptionBox
